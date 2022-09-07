e257a80ed9afe1920161e62fb8386329f7397880
Kevin Trickey <registrations@kevintrickey.me.uk>
2022-09-07 06:26:28 +0100

    Update playwright.spec.ts

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
-  await page.waitForSelector('h1');
+  await page.waitForSelector('a.navbar-brand');
   await expect(page.locator('a.navbar-brand')).toContainText('Kevin Trickey');
 })
