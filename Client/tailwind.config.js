7c524edb6f820244d4b0a73ba3e25c2a590a99bb
kevin.trickey <registrations@kevintrickey.me.uk>
2022-09-26 17:50:43 +0100

    update to get new theme working

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{razor,html,cshtml}"],
+  darkMode: 'class',
+  content: ["./**/*.{razor,html,cshtml}"],
   theme: {
     extend: {},
   },
