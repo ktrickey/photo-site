9481fd7ae27d161ffc43510ebb098c6f65d10a7b
kevin.trickey <registrations@kevintrickey.me.uk>
2022-11-28 19:56:25 +0000

    endpoints for api

---
+++ ApiIsolated/Gallery/GalleryGetAll.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ApiIsolated.Gallery
+{
+    public class HttpTrigger
+    {
+
+        private readonly ILogger _logger;
+
+        public HttpTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HttpTrigger>();
+        }
+
+        [Function("getFullGallery")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "gallery")] HttpRequestData req)
+        {
+            if (!Directory.Exists("home"))
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(GalleryStructure.Structure);
+            return response;
+        }
+
+
+
+    }
+}
---
+++ ApiIsolated/Gallery/GalleryGetByName.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace ApiIsolated.Gallery;
+
+public class GalleryGetByName
+{
+
+    [Function("GalleryGetByName")]
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "gallery/{name}")] HttpRequestData req, string name)
+    {
+        var found = GalleryStructure.Structure.Find(name).ToString();
+        if (!Directory.Exists("home") || !found.Any())
+        {
+            return req.CreateResponse(HttpStatusCode.NotFound);
+        }
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteAsJsonAsync(found);
+        return response;
+    }
+}
