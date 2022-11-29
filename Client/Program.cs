06e02aff6360997fe8bed82e76d415bdb11a3869
kevin.trickey <registrations@kevintrickey.me.uk>
2022-11-29 22:17:29 +0000

    mud blazor implementation

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,11 +2,12 @@ using System.Collections.Immutable;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
-using Syncfusion.Blazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSyncfusionBlazor();
+builder.Services.AddMudServices();
+
 await builder.Build().RunAsync();
